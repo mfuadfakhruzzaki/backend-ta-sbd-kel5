c42e971cfeba363ba11888df0b86b09b153a913c
mfuadfakhruzzaki <mfuadfakhruzzaki@students.undip.ac.id>
2025-04-02 02:09:02 +0700

    Update database and Appwrite configuration settings in LoadConfig function; change default values for DB_HOST, DB_PASSWORD, DB_NAME, JWT_SECRET, and Appwrite endpoint, project ID, API key, and bucket ID.

--- internal/config/config.go
+++ internal/config/config.go
@@ -71,15 +71,15 @@ func LoadConfig() (*Config, error) {
 	env := getEnv("APP_ENV", "development")
 
 	// Konfigurasi database
-	dbHost := getEnv("DB_HOST", "localhost")
+	dbHost := getEnv("DB_HOST", "postgres")
 	dbPort := getEnv("DB_PORT", "5432")
 	dbUser := getEnv("DB_USER", "postgres")
-	dbPassword := getEnv("DB_PASSWORD", "postgres")
-	dbName := getEnv("DB_NAME", "jual_beli_db")
+	dbPassword := getEnv("DB_PASSWORD", "tasbdkel5")
+	dbName := getEnv("DB_NAME", "jubel_db")
 	dbSSLMode := getEnv("DB_SSLMODE", "disable")
 
 	// Konfigurasi JWT
-	jwtSecret := getEnv("JWT_SECRET", "rahasia_jwt_sangat_aman")
+	jwtSecret := getEnv("JWT_SECRET", "kelompok5tasbd")
 	jwtExpiryStr := getEnv("JWT_EXPIRY", "24h")
 	jwtExpiry, err := time.ParseDuration(jwtExpiryStr)
 	if err != nil {
@@ -95,10 +95,10 @@ func LoadConfig() (*Config, error) {
 	}
 
 	// Konfigurasi Appwrite
-	appwriteEndpoint := getEnv("APPWRITE_ENDPOINT", "http://tasbd-appwrite-38c346-34-101-66-9.traefik.me/v1")
-	appwriteProjectID := getEnv("APPWRITE_PROJECT_ID", "67e7bbfb003b2a88a380")
-	appwriteAPIKey := getEnv("APPWRITE_API_KEY", "standard_cb368bd976b49d276ae32ee22b541ab37799d406326f72aab5499c9b9a4adebaefd92489e80fab8fa66f255b80ed9c0f8b0829d52b1e67779f584e42c2b0ffff4280abf621e17c588ab1523659d36515d2fc741fba1f2b8d24e4383b3319b69e125dfb38689dbeb90722e86f37a4ce3ec25a305a88d9a18c260ad128404cd27f")
-	appwriteBucketID := getEnv("APPWRITE_BUCKET_ID", "67e7bc05000d4dde5eb1")
+	appwriteEndpoint := getEnv("APPWRITE_ENDPOINT", "http://tugas-akhir-sbd-appwrite-baa3ca-34-50-95-184.traefik.me")
+	appwriteProjectID := getEnv("APPWRITE_PROJECT_ID", "67ec168f001774966f85")
+	appwriteAPIKey := getEnv("APPWRITE_API_KEY", "standard_c198465575b7925e70d344fe9b76414e306f0d2f3d6137371e23a36bbf48c0d89f884981271b0f7de15b9d2162badf9cdaab963560bc99cc6278e99b02f17cb1d08d5cd81140c8c09397fcf503a3a2c1f4dd2056f46eb57d16bdd315f2b74e38600bd82e0ab061eb1786697cf7c1aa94e7a1bed5d0747f7e4fec6ac0bfb602f3")
+	appwriteBucketID := getEnv("APPWRITE_BUCKET_ID", "67ec16ad001dd1f0a484")
 
 	// Pastikan direktori upload ada
 	if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
