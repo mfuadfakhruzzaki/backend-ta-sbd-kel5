8e282e81f8ad8e6c34b2cb78a5689f50f7532f92
mfuadfakhruzzaki <mfuadfakhruzzaki@students.undip.ac.id>
2025-04-02 03:32:01 +0700

    Update Appwrite endpoint in configuration files; modify docker-compose.yml and LoadConfig function to include the correct API version in the endpoint URL for improved service integration.

--- internal/config/config.go
+++ internal/config/config.go
@@ -95,7 +95,7 @@ func LoadConfig() (*Config, error) {
 	}
 
 	// Konfigurasi Appwrite
-	appwriteEndpoint := getEnv("APPWRITE_ENDPOINT", "http://tugas-akhir-sbd-appwrite-baa3ca-34-50-95-184.traefik.me")
+	appwriteEndpoint := getEnv("APPWRITE_ENDPOINT", "http://tugas-akhir-sbd-appwrite-baa3ca-34-50-95-184.traefik.me/v1")
 	appwriteProjectID := getEnv("APPWRITE_PROJECT_ID", "67ec168f001774966f85")
 	appwriteAPIKey := getEnv("APPWRITE_API_KEY", "standard_c198465575b7925e70d344fe9b76414e306f0d2f3d6137371e23a36bbf48c0d89f884981271b0f7de15b9d2162badf9cdaab963560bc99cc6278e99b02f17cb1d08d5cd81140c8c09397fcf503a3a2c1f4dd2056f46eb57d16bdd315f2b74e38600bd82e0ab061eb1786697cf7c1aa94e7a1bed5d0747f7e4fec6ac0bfb602f3")
 	appwriteBucketID := getEnv("APPWRITE_BUCKET_ID", "67ec16ad001dd1f0a484")
